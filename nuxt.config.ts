3cc69420b1ae0c4601d23553bc095931201fa512
Lumethys <lekhang8@gmail.com>
2024-11-14 23:26:11 +0700

    add i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,29 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+
   future: {
     compatibilityVersion: 4
+  },
+
+  modules: ['@nuxtjs/i18n'],
+
+  i18n: {
+    // locales: [
+    //   { code: 'en', language: 'en-US' },
+    //   { code: 'ja', language: 'ja-JP' }
+    // ],
+    locales: [
+      {
+        code: 'en',
+        file: 'en-US.ts'
+      },
+      {
+        code: 'ja',
+        file: 'ja-JP.ts'
+      },
+    ],
+    lazy: true,
+    defaultLocale: 'en',
   }
 })
